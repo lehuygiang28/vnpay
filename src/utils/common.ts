e8499e31cd693acd3266ec8b6d908bfe39b0e6ba
Lê Huy Giang <lehuygiang28@gmail.com>
2023-12-11 13:34:55 +0700

    Refactor resolveUrlString function to trim and normalize host and path (#3)

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -60,5 +60,13 @@ export function getResponseByStatusCode(
 }
 
 export function resolveUrlString(host: string, path: string): string {
-    return `${host.replace(/\/+$/, '')}/${path.replace(/^\//, '')}`;
+    host = host.trim();
+    path = path.trim();
+    while (host.endsWith('/') || host.endsWith('\\')) {
+        host = host.slice(0, -1);
+    }
+    while (path.startsWith('/') || path.startsWith('\\')) {
+        path = path.slice(1);
+    }
+    return `${host}/${path}`;
 }
