9bcc8f0e46b98bf2e0b3268551d1d41c111dacf0
lehuygiang28 <lehuygiang28@gmail.com>
2024-05-21 10:04:18 +0700

    refactor(logger): add jsdocs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,31 @@
 import * as fs from 'fs';
 
+/**
+ * Truyền vào `loggerFn` để bỏ qua logger
+ *
+ * @en Pass to `loggerFn` for ignoring logger
+ * @returns {void}
+ */
+export function ignoreLogger(): void {}
+
+/**
+ * Ghi dữ liệu ra console
+ *
+ * @en Log data to console
+ * @param data - Data to be logged
+ */
 export function consoleLogger(data: unknown): void {
     console.log(data);
 }
 
+/**
+ * Ghi dữ liệu ra file
+ *
+ * @en Log data to file
+ * @param data Data to be logged
+ * @param filePath File path to be written
+ * @param errorCallback Error callback function
+ */
 export function fileLogger(data: unknown, filePath: string, errorCallback?: unknown): void {
     const dataString = typeof data === 'object' ? JSON.stringify(data) : String(data);
     fs.appendFile(filePath, dataString + '\n', (err) => {
