b0e53bb3b6007322433d1c5135efa7b979b4a331
lehuygiang28 <lehuygiang28@gmail.com>
2025-11-11 11:45:34 +0700

    chore: :sparkles: update Jest configuration and dependencies
    - Added a transform configuration for TypeScript files in Jest to use a specific tsconfig for testing.
    - Updated @types/jest dependency from version 29.5.5 to 29.5.14 for improved type definitions.
    - Introduced a new tsconfig.test.json file to define TypeScript settings for the test environment.

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,15 @@ process.env.TZ = 'UTC';
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    transform: {
+        // match with .ts or .tsx files
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.test.json',
+            },
+        ],
+    },
     coverageThreshold: {
         global: {
             lines: 80,
